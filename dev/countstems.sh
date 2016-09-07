bb6982b1d093f082effcda9d464f4ff6724eabc5
Francis M. Tyers <ftyers@prompsit.com>
2016-09-07 22:03:08 +0000

    stuffs

--- dev/countstems.sh
+++ dev/countstems.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-LEXC=../apertium-uig.uig.lexc
-
-allcounts=`foma -e "read lexc $LEXC" -e "quit" | grep Root | sed 's/,/\n/g'`
-
-for line in $allcounts; do
-	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
-	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
-	if [[ "$thing" == "Root" ]]; then
-		rootsize=$numbr;
-		root=`grep -A$rootsize Root $LEXC | grep -v Root`;
-	fi;
-done;
-
-root=`echo $root | sed -r 's/\s*;/\n/g'`;
-
-string=""
-for line in $allcounts; do
-	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
-	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
-	for thingname in $root; do
-		if [ "$thing" = "$thingname" ]; then
-			string="$string + $numbr";
-		fi;
-	done;
-done;
-
-echo `calc $string`;
+foma -e "read lexc ../apertium-uig.uig.lexc" -e "quit" | grep Root | sed 's/,/\n/g' | grep '[A-Z][a-z]\+' | grep -v 'Root' | grep -o '[0-9]\+$' | sed 's/$/+/g' | tr -d '\n' | sed 's/\+$//g' | calc -p
